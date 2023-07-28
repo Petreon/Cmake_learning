618e3094311d3d236948322eb74e37b5f79cec93
petreon <vitorr2009@gmail.com>
2023-07-27 21:27:59 -0300

    Changing files from linux

--- Epp02/source/main.cpp
+++ Epp02/source/main.cpp
@@ -11,7 +11,7 @@ T add(T a, T b) {
 int main() {
 
     std::cout << "Hello from C++ 20 with Cmake on Linux/Windows" << std::endl;
-    std::cout << "The sum is : " << add(7,5) << std::endl;
+    std::cout << "The sum is : " << add(7,3) << std::endl;
 
     return 0;
 }
